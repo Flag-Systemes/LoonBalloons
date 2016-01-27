481de3348ab0b57f0f2c5fa06ca77d02edf7dc5c
Csluikidikilest <csluikidikilest@gmail.com>
2016-01-27 20:41:26 +0100

    Initial Commit

---
+++ LoonBalloons/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoonBalloons.Utils;
+
+namespace LoonBalloons
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string test;
+
+      test = "2,1 5 1 2 5";
+      List<decimal> liste1 = test.Split<decimal>(' ');
+
+      test = "2 1 5 1 2 5";
+      List<int> liste2 = test.Split<int>(' ');
+
+      Console.ReadLine();
+    }
+  }
+}
