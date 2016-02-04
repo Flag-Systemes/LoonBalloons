784d71a25e570b63219271e4e51a2b5ccf98411b
benoit mira <benoit.mira@gmail.com>
2016-02-04 20:39:36 +0100

    informations

---
+++ LoonBalloons/Utils/Informations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoonBalloons.Utils
+{
+    public class Informations
+    {
+        public Informations()
+        {
+            resultFile = FileUtils.In;
+        }
+
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+
+        public int Altitude { get; set; }
+
+        public List<Tuple<int, int>> Winds { get; set; }
+
+        public int NbTour { get; set; }
+
+        public int Ballons { get; set; }
+
+        List<string> resultFile = new List<string>();
+    }
+}
