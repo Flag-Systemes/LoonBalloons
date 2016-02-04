79d56463ef10b7ba45063723f2958460e5ec9cf1
Csluikidikilest <csluikidikilest@gmail.com>
2016-02-04 20:35:01 +0100

    Cell & Grid

---
+++ LoonBalloons/Data/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoonBalloons.Data
+{
+  public class Cell
+  {
+    public List<Cell> AdjacentsCells { get; set; }
+
+    public int X { get; set; }
+
+    public int Y { get; set; }
+    
+    public Dictionary<int, Tuple<int, int>> Winds { get; set; }
+
+
+  }
+}
---
+++ LoonBalloons/Data/Grid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoonBalloons.Data
+{
+  public class Grid : List<Cell>
+  {
+    public int Altitude { get; set; }
+
+    public int ColsCount { get; set; }
+
+    public int RowsCount { get; set; }
+
+    public List<Cell> AdjacentsCells(Cell cell)
+    {
+      if(cell.AdjacentsCells == null)
+      {
+        cell.AdjacentsCells = this.Where(item => ((item.Y == cell.Y) && (item.X == cell.X - 1 || item.X == cell.X + 1)) || ((item.X == cell.X) && (item.Y == cell.Y - 1 || item.Y == cell.Y + 1))).ToList();
+      }
+      return cell.AdjacentsCells;
+    }
+  }
+}
